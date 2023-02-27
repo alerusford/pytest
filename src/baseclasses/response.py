983585eec8f786848d8ecd38b246e09458c6c626
alexandr <alerusford@gmail.com>
2023-02-27 18:28:48 +0300

    asus_3_3 - ok

--- src/baseclasses/response.py
+++ src/baseclasses/response.py
@@ -4,7 +4,7 @@ from src.enums.global_enums import GlobalErrorMessages
 class Response:
     def __init__(self, response):
         self.response = response
-        self.response_json = response.json()
+        self.response_json = response.json().get('data')
         self.response_status_code = response.status_code
 
     def validate(self, schema):
@@ -18,7 +18,10 @@ class Response:
 
     def assert_status_code(self, status_code):
         if isinstance(status_code, list):
-            assert self.response_status_code in status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
+            assert self.response_status_code in status_code, self
         else:
-            assert self.response_status_code == status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
+            assert self.response_status_code == status_code, self
         return self
+
+    def __str__(self):
+        return f'\nStatus code: {self.response_status_code} \nRequested url: {self.response.url}'
--- tests/something_test.py
+++ tests/something_test.py
@@ -1,10 +1,10 @@
 import requests
 from config import SERVICE_URL
 from src.baseclasses.response import Response
-# from src.schemas.post import POST_SCHEMA
-from src.pydantic_schemas.post import Post
+from src.schemas.user import User
 
-def test_getting_posts():
-    r = requests.get(url=SERVICE_URL)
-    response = Response(r)
-    response.assert_status_code(200).validate(Post)
+def test_getting_users_list():
+    response = requests.get(SERVICE_URL)
+    text_object = Response(response)
+    text_object.assert_status_code(300).validate(User)
+
